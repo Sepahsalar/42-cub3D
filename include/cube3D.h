8a1efe70f1c4377c13ebb8d498fd4c3c398d6667
nima-nourinejad <nima.nourinejad@gmail.com>
2024-07-19 12:01:39 +0300

    added base

---
+++ include/cube3D.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cube3D.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/19 12:00:15 by nnourine          #+#    #+#             */
+/*   Updated: 2024/07/19 12:01:05 by nnourine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef cube3D_H
+# define cube3D_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <math.h>
+# include "../lib/libft/libft.h"
+# include "../lib/ft_printf/ft_printf.h"
+# include "../lib/MLX42/include/MLX42/MLX42.h"
+
+#endif // cube3D_H
