5424903be055c5785964a0347d5ac38f03e310be
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-08-09 18:04:14 +0300

    making sure dis left and right are same

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asohrabi <asohrabi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 13:43:59 by nnourine          #+#    #+#             */
-/*   Updated: 2024/08/09 17:10:04 by asohrabi         ###   ########.fr       */
+/*   Updated: 2024/08/09 18:01:28 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -243,34 +243,34 @@ double height(double distance, char c)
 		return ((WINDOW_HEIGHT * target_height / (2 * half_full_height)));
 }
 
-double find_right_distance(t_all *all)
-{
-	t_loc	*temp_loc;
-	double	temp_distance;
-	double	x_player;
-	double	y_player;
-	double 	max_d;
-	double 	x_min;
-	double 	x_max;
-	int 	x_size_game;
+// double find_right_distance(t_all *all)
+// {
+// 	t_loc	*temp_loc;
+// 	double	temp_distance;
+// 	double	x_player;
+// 	double	y_player;
+// 	double 	max_d;
+// 	double 	x_min;
+// 	double 	x_max;
+// 	int 	x_size_game;
 	
-	x_player = start_loc_player(all, 'x');
-	y_player = start_loc_player(all, 'y');
-	max_d = max_distance(all);
-	x_size_game = game_size(all, 'x');
-	x_min = min_x(x_player, 360);
-	x_max = max_x(x_player, x_size_game, 360);
-	temp_distance = max_d;
-	temp_loc = all->map->start;
-	while (temp_loc)
-	{
-		if (temp_loc->y_mid == y_player && temp_loc->x_mid <= x_max && temp_loc->x_mid >= x_min && temp_loc->c == '1')
-			if (fabs(temp_loc->x_mid - x_player) < temp_distance)
-				temp_distance = fabs(temp_loc->x_mid - x_player);
-		temp_loc = temp_loc->next;
-	}
-	return (temp_distance);
-}
+// 	x_player = start_loc_player(all, 'x');
+// 	y_player = start_loc_player(all, 'y');
+// 	max_d = max_distance(all);
+// 	x_size_game = game_size(all, 'x');
+// 	x_min = min_x(x_player, 360);
+// 	x_max = max_x(x_player, x_size_game, 360);
+// 	temp_distance = max_d;
+// 	temp_loc = all->map->start;
+// 	while (temp_loc)
+// 	{
+// 		if (temp_loc->y_mid == y_player && temp_loc->x_mid <= x_max && temp_loc->x_mid >= x_min && temp_loc->c == '1')
+// 			if (fabs(temp_loc->x_mid - x_player) < temp_distance)
+// 				temp_distance = fabs(temp_loc->x_mid - x_player);
+// 		temp_loc = temp_loc->next;
+// 	}
+// 	return (temp_distance);
+// }
 
 int same(double d1, int d2)
 {
@@ -432,6 +432,365 @@ int same_double(double d1, double d2)
 // 	}
 // }
 
+double find_right_distance(t_all *all)
+{
+	// t_loc		*temp_loc;
+	// double		temp_x;
+	// double		temp_y;
+	double		x_player;
+	double		y_player;
+	double		angle_player;
+	double		temp_angle;
+	double		ray_angle;
+	int 		x_size_game;
+	int 		y_size_game;
+	// int			x_min;
+	// int			x_max;
+	// int			y_min;
+	// int			y_max;
+	// double		temp_x_min;
+	// double		temp_y_min;
+	// double		temp_x_max;
+	// double		temp_y_max;
+	// int 		i;
+	// double 		max_d;
+	// char 		type_intersection;
+	// double 		winner_distance;
+	// double		temp_dis;
+	double		intersection_distance;
+	t_range		range;
+	t_possible *temp_pos;
+	t_possible	*winner;
+	char		winner_material;
+
+	x_player = all->x;
+	y_player = all->y;
+	angle_player = all->angle;
+	x_size_game = all->map_width;
+	y_size_game = all->map_height;
+	// max_d = all->max_distance;
+	temp_angle = 180;
+	ray_angle = under_full_circle(angle_player - (HAOV / 2) + temp_angle);
+	intersection_distance = 0;
+	// temp_x_min = min_x(x_player, ray_angle);
+	// temp_x_max = max_x(x_player, x_size_game, ray_angle);
+	// temp_y_min = min_y(y_player, ray_angle);
+	// temp_y_max = max_y(y_player, y_size_game, ray_angle);
+	// printf("temp_min_x: %f, temp_y_min: %f, temp_x_max:%f, temp_y_max:%f\n", temp_x_min, temp_y_min, temp_x_max, temp_y_max);
+	// // winner_distance = max_d;
+	// if (same(temp_x_min, 0))
+	// {
+	// 	range.x_min = 0;
+	// 	range.x_max = ceil(x_player);
+	// }
+	// else
+	// {
+	// 	range.x_min = floor(temp_x_min);
+	// 	range.x_max = all->map_width - 1;
+	// }
+	// if (same(temp_y_min , 0))
+	// {
+	// 	range.y_min = 0;
+	// 	range.y_max = ceil(y_player);
+	// }
+	// else
+	// {
+	// 	range.y_min = floor(range.x_min);
+	// 	range.y_max = all->map_height - 1;
+	// }
+	// printf("min_x: %d, y_min: %d, x_max:%d, y_max:%d\n", range.x_min, range.y_min, range.x_max, range.y_max);
+	range.x_min = min_x(x_player, ray_angle);
+	range.x_max = max_x(x_player, x_size_game, ray_angle);
+	range.y_min = min_y(y_player, ray_angle);
+	range.y_max = max_y(y_player, y_size_game, ray_angle);
+	create_possibility(all, range);
+	find_winner_surface(all);
+	winner = NULL;
+	temp_pos = all->possible;
+	if (same(ray_angle, 90) || same(ray_angle, 270))
+	{
+		if (same(ray_angle, 270))
+		{
+			while (temp_pos)
+			{
+				if (temp_pos->sn_winner_texture == 'S' && same_double(temp_pos->sn_winner_x, all->x))
+				{
+					if (!winner)
+						winner = temp_pos;
+					else if (temp_pos->sn_winner_distance < winner->sn_winner_distance)
+						winner = temp_pos;
+				}
+				temp_pos = temp_pos->next;
+			}
+			winner_material = 'S';
+		}
+		else
+		{
+			while (temp_pos)
+			{
+				if (temp_pos->sn_winner_texture == 'N' && same_double(temp_pos->sn_winner_x, all->x))
+				{
+					if (!winner)
+						winner = temp_pos;
+					else if (temp_pos->sn_winner_distance < winner->sn_winner_distance)
+						winner = temp_pos;
+				}
+				temp_pos = temp_pos->next;
+			}
+			winner_material = 'N';			
+		}
+		intersection_distance = winner->sn_winner_distance;
+
+	}
+	else if (same(ray_angle, 0) || same(ray_angle, 180))
+	{
+		if (same(ray_angle, 0))
+		{
+			while (temp_pos)
+			{
+				if (temp_pos->ew_winner_texture == 'W' && same_double(temp_pos->ew_winner_y, all->y))
+				{
+					if (!winner)
+						winner = temp_pos;
+					else if (temp_pos->ew_winner_distance < winner->ew_winner_distance)
+						winner = temp_pos;
+				}
+				temp_pos = temp_pos->next;
+			}
+			winner_material = 'W';
+		}
+		else
+		{
+			while (temp_pos)
+			{
+				if (temp_pos->ew_winner_texture == 'E' && same_double(temp_pos->ew_winner_y, all->y))
+				{
+					if (!winner)
+						winner = temp_pos;
+					else if (temp_pos->ew_winner_distance < winner->ew_winner_distance)
+						winner = temp_pos;
+				}
+				temp_pos = temp_pos->next;
+			}
+			winner_material = 'E';
+		}
+		intersection_distance = winner->ew_winner_distance;
+	}
+	else
+	{
+		while (temp_pos)
+		{
+			if (fabs(find_y(ray_angle, all->x, all->y, temp_pos->ew_winner_x) - temp_pos->ew_winner_y) < 1)
+			{
+				if (!winner)
+					winner = temp_pos;
+				else if (temp_pos->ew_winner_distance < winner->ew_winner_distance)
+					winner = temp_pos;
+				winner_material = winner->ew_winner_texture;
+			}
+			else if (fabs(find_x(ray_angle, all->x, all->y, temp_pos->sn_winner_y) - temp_pos->sn_winner_x) < 1)
+			{
+				if (!winner)
+					winner = temp_pos;
+				else if (temp_pos->sn_winner_distance < winner->sn_winner_distance)
+					winner = temp_pos;
+				winner_material = winner->sn_winner_texture;
+			}
+			temp_pos = temp_pos->next;
+		}
+	}
+	if (winner_material == winner->ew_winner_texture)
+		intersection_distance = distance(all->x, all->y, winner->ew_winner_x, find_y(ray_angle, all->x, all->y, winner->ew_winner_x));
+	else
+		intersection_distance = distance(all->x, all->y, find_x(ray_angle, all->x, all->y, winner->sn_winner_y), winner->sn_winner_y);
+	
+	if (intersection_distance < 0.5)
+		intersection_distance = 0.5;
+	clean_possibility(all->possible);
+	all->possible = NULL;
+	return (intersection_distance);
+}
+
+double find_left_distance(t_all *all)
+{
+	// t_loc		*temp_loc;
+	// double		temp_x;
+	// double		temp_y;
+	double		x_player;
+	double		y_player;
+	double		angle_player;
+	double		temp_angle;
+	double		ray_angle;
+	int 		x_size_game;
+	int 		y_size_game;
+	// int			x_min;
+	// int			x_max;
+	// int			y_min;
+	// int			y_max;
+	// double		temp_x_min;
+	// double		temp_y_min;
+	// double		temp_x_max;
+	// double		temp_y_max;
+	// int 		i;
+	// double 		max_d;
+	// char 		type_intersection;
+	// double 		winner_distance;
+	// double		temp_dis;
+	double		intersection_distance;
+	t_range		range;
+	t_possible *temp_pos;
+	t_possible	*winner;
+	char		winner_material;
+
+	x_player = all->x;
+	y_player = all->y;
+	angle_player = all->angle;
+	x_size_game = all->map_width;
+	y_size_game = all->map_height;
+	// max_d = all->max_distance;
+	temp_angle = 0;
+	ray_angle = under_full_circle(angle_player - (HAOV / 2) + temp_angle);
+	intersection_distance = 0;
+	// temp_x_min = min_x(x_player, ray_angle);
+	// temp_x_max = max_x(x_player, x_size_game, ray_angle);
+	// temp_y_min = min_y(y_player, ray_angle);
+	// temp_y_max = max_y(y_player, y_size_game, ray_angle);
+	// printf("temp_min_x: %f, temp_y_min: %f, temp_x_max:%f, temp_y_max:%f\n", temp_x_min, temp_y_min, temp_x_max, temp_y_max);
+	// // winner_distance = max_d;
+	// if (same(temp_x_min, 0))
+	// {
+	// 	range.x_min = 0;
+	// 	range.x_max = ceil(x_player);
+	// }
+	// else
+	// {
+	// 	range.x_min = floor(temp_x_min);
+	// 	range.x_max = all->map_width - 1;
+	// }
+	// if (same(temp_y_min , 0))
+	// {
+	// 	range.y_min = 0;
+	// 	range.y_max = ceil(y_player);
+	// }
+	// else
+	// {
+	// 	range.y_min = floor(range.x_min);
+	// 	range.y_max = all->map_height - 1;
+	// }
+	// printf("min_x: %d, y_min: %d, x_max:%d, y_max:%d\n", range.x_min, range.y_min, range.x_max, range.y_max);
+	range.x_min = min_x(x_player, ray_angle);
+	range.x_max = max_x(x_player, x_size_game, ray_angle);
+	range.y_min = min_y(y_player, ray_angle);
+	range.y_max = max_y(y_player, y_size_game, ray_angle);
+	create_possibility(all, range);
+	find_winner_surface(all);
+	winner = NULL;
+	temp_pos = all->possible;
+	if (same(ray_angle, 90) || same(ray_angle, 270))
+	{
+		if (same(ray_angle, 270))
+		{
+			while (temp_pos)
+			{
+				if (temp_pos->sn_winner_texture == 'S' && same_double(temp_pos->sn_winner_x, all->x))
+				{
+					if (!winner)
+						winner = temp_pos;
+					else if (temp_pos->sn_winner_distance < winner->sn_winner_distance)
+						winner = temp_pos;
+				}
+				temp_pos = temp_pos->next;
+			}
+			winner_material = 'S';
+		}
+		else
+		{
+			while (temp_pos)
+			{
+				if (temp_pos->sn_winner_texture == 'N' && same_double(temp_pos->sn_winner_x, all->x))
+				{
+					if (!winner)
+						winner = temp_pos;
+					else if (temp_pos->sn_winner_distance < winner->sn_winner_distance)
+						winner = temp_pos;
+				}
+				temp_pos = temp_pos->next;
+			}
+			winner_material = 'N';			
+		}
+		intersection_distance = winner->sn_winner_distance;
+
+	}
+	else if (same(ray_angle, 0) || same(ray_angle, 180))
+	{
+		if (same(ray_angle, 0))
+		{
+			while (temp_pos)
+			{
+				if (temp_pos->ew_winner_texture == 'W' && same_double(temp_pos->ew_winner_y, all->y))
+				{
+					if (!winner)
+						winner = temp_pos;
+					else if (temp_pos->ew_winner_distance < winner->ew_winner_distance)
+						winner = temp_pos;
+				}
+				temp_pos = temp_pos->next;
+			}
+			winner_material = 'W';
+		}
+		else
+		{
+			while (temp_pos)
+			{
+				if (temp_pos->ew_winner_texture == 'E' && same_double(temp_pos->ew_winner_y, all->y))
+				{
+					if (!winner)
+						winner = temp_pos;
+					else if (temp_pos->ew_winner_distance < winner->ew_winner_distance)
+						winner = temp_pos;
+				}
+				temp_pos = temp_pos->next;
+			}
+			winner_material = 'E';
+		}
+		intersection_distance = winner->ew_winner_distance;
+	}
+	else
+	{
+		while (temp_pos)
+		{
+			if (fabs(find_y(ray_angle, all->x, all->y, temp_pos->ew_winner_x) - temp_pos->ew_winner_y) < 1)
+			{
+				if (!winner)
+					winner = temp_pos;
+				else if (temp_pos->ew_winner_distance < winner->ew_winner_distance)
+					winner = temp_pos;
+				winner_material = winner->ew_winner_texture;
+			}
+			else if (fabs(find_x(ray_angle, all->x, all->y, temp_pos->sn_winner_y) - temp_pos->sn_winner_x) < 1)
+			{
+				if (!winner)
+					winner = temp_pos;
+				else if (temp_pos->sn_winner_distance < winner->sn_winner_distance)
+					winner = temp_pos;
+				winner_material = winner->sn_winner_texture;
+			}
+			temp_pos = temp_pos->next;
+		}
+	}
+	if (winner_material == winner->ew_winner_texture)
+		intersection_distance = distance(all->x, all->y, winner->ew_winner_x, find_y(ray_angle, all->x, all->y, winner->ew_winner_x));
+	else
+		intersection_distance = distance(all->x, all->y, find_x(ray_angle, all->x, all->y, winner->sn_winner_y), winner->sn_winner_y);
+	
+	if (intersection_distance < 0.5)
+		intersection_distance = 0.5;
+	clean_possibility(all->possible);
+	all->possible = NULL;
+	return (intersection_distance);
+}
+
+
 void	size_grid(t_all *all)
 {
 	// t_loc		*temp_loc;
@@ -475,6 +834,8 @@ void	size_grid(t_all *all)
 	// max_d = all->max_distance;
 	temp_angle = 0;
 	counter = 0;
+	distance_left = find_left_distance(all);
+	distance_right = find_right_distance(all);
 	while (counter <= NLOOP)
 	{
 		ray_angle = under_full_circle(angle_player - (HAOV / 2) + temp_angle);
@@ -613,13 +974,28 @@ void	size_grid(t_all *all)
 		
 		if (intersection_distance < 0.5)
 			intersection_distance = 0.5;
-		if (same(temp_angle, 0))
-			distance_left = intersection_distance;
-		distance_right = find_right_distance(all);
-		if (temp_angle >= 0 && temp_angle <= 90)
-			intersection_distance = intersection_distance * fabs(ft_sin(temp_angle)) + distance_left;
-		else
-			intersection_distance = intersection_distance * fabs(ft_sin(temp_angle)) + distance_right;
+		// if (same(temp_angle, 0))
+		// 	distance_left = intersection_distance;
+		// distance_right = find_right_distance(all);
+		// if (temp_angle >= 0 && temp_angle <= 90)
+		// 	intersection_distance = intersection_distance * fabs(ft_sin(temp_angle)) + distance_left;
+		// else
+		// 	intersection_distance = intersection_distance * fabs(ft_sin(temp_angle)) + distance_right;
+		// if (counter == 0)
+		// 	distance_left = intersection_distance;
+		// else if (counter == 1)
+		// 	distance_right = intersection_distance;
+		// // distance_right = find_right_distance(all);
+		// else
+		// {
+			if (temp_angle >= 0 && temp_angle <= 90)
+				intersection_distance = intersection_distance * fabs(ft_sin(temp_angle)) + distance_left;
+			else
+				intersection_distance = intersection_distance * fabs(ft_sin(temp_angle)) + distance_right;
+			printf("distance left: %f and distance right: %f\n", distance_left, distance_right);
+		// }
+		// printf("intersection_distance: %f\n", intersection_distance);
+		// printf("temp_angle: %f\n", temp_angle);
 		data_rander.wall_texture = winner_material;
 		data_rander.floor_height = height(intersection_distance, 'F');
 		data_rander.ceil_height = height(intersection_distance, 'C');
