f81bbbf52ab0743c25156fc7b9ab86ecae095abd
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-08-14 12:15:40 +0300

    map parser was corrected

--- src/map_parser.c
+++ src/map_parser.c
@@ -6,32 +6,56 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 11:09:57 by nnourine          #+#    #+#             */
-/*   Updated: 2024/08/14 11:10:36 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/08/14 12:15:17 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
 
+// void	update_map(t_all *all, char *found)
+// {
+// 	char	*end_part1;
+// 	char	*start_part2;
+// 	char	*temp;
+// 	int		len1;
+// 	int		len2;
+
+// 	end_part1 = all->strmap;
+// 	temp = all->strmap;
+// 	while(temp < found)
+// 	{
+// 		if (*temp == '\n')
+// 			end_part1 = temp;
+// 		temp++;
+// 	}
+// 	while (*found != '\n')
+// 		found++;
+// 	start_part2 = found + 1;
+// 	len1 = end_part1 - all->strmap;
+// 	len2 = ft_strlen(start_part2);
+// 	temp = malloc(len1 + len2 + 1);
+// 	check_failure(0, temp, 2, all);
+// 	ft_memcpy(temp, all->strmap, len1);
+// 	ft_memcpy(temp + len1, start_part2, len2 + 1);
+// 	free(all->strmap);
+// 	all->strmap = temp;
+// }
+
 void	update_map(t_all *all, char *found)
 {
+	char	*start_part1;
 	char	*end_part1;
 	char	*start_part2;
 	char	*temp;
 	int		len1;
 	int		len2;
 
-	end_part1 = all->strmap;
-	temp = all->strmap;
-	while(temp < found)
-	{
-		if (*temp == '\n')
-			end_part1 = temp;
-		temp++;
-	}
-	while (*found != '\n')
-		found++;
-	start_part2 = found + 1;
-	len1 = end_part1 - all->strmap;
+	start_part1 = all->strmap;
+	end_part1 = found;
+	start_part2 = found;
+	while(*start_part2 && *start_part2 != '\n')
+		start_part2++;
+	len1 = end_part1 - start_part1;
 	len2 = ft_strlen(start_part2);
 	temp = malloc(len1 + len2 + 1);
 	check_failure(0, temp, 2, all);
@@ -41,35 +65,67 @@ void	update_map(t_all *all, char *found)
 	all->strmap = temp;
 }
 
+// char	*custom_strdup(t_all *all, char *found, char *str)
+// {
+// 	char	*start;
+// 	char	*end;
+// 	int		len;
+// 	char	*dup;
+
+// 	start = found;
+// 	while (*start != ' ')
+// 		start++;
+// 	while (*start == ' ')
+// 		start++;
+// 	end = start;
+// 	while (end && *end != ' ' && *end != '\n')
+// 		end++;
+// 	len = end - start;
+// 	if (len == 0)
+// 	{
+// 		ft_putstr_fd("There is no information for ", 2);
+// 		ft_putstr_fd(str, 2);
+// 		ft_putendl_fd(" identifier in the map", 2);
+// 		terminate(all, 1);
+// 	}
+// 	dup = malloc(len + 1);
+// 	check_failure(0, dup, 2, all);
+// 	ft_memcpy(dup, start, len);
+// 	dup[len] = '\0';
+// 	update_map(all, found);
+// 	return (dup);    
+// }
+
 char	*custom_strdup(t_all *all, char *found, char *str)
 {
 	char	*start;
 	char	*end;
 	int		len;
 	char	*dup;
+	char	*temp;
 
 	start = found;
 	while (*start != ' ')
 		start++;
-	while (*start == ' ')
-		start++;
 	end = start;
-	while (end && *end != ' ' && *end != '\n')
+	while (*end && *end != '\n')
 		end++;
-	len = end - start;
-	if (len == 0)
+	len = end - start;	
+	dup = malloc(len + 1);
+	check_failure(0, dup, 2, all);
+	ft_memcpy(dup, start, len);
+	dup[len] = '\0';
+	update_map(all, found);
+	temp = ft_strtrim(dup, " ");
+	free(dup);
+	if (ft_strlen(temp) == 0)
 	{
 		ft_putstr_fd("There is no information for ", 2);
 		ft_putstr_fd(str, 2);
 		ft_putendl_fd(" identifier in the map", 2);
 		terminate(all, 1);
 	}
-	dup = malloc(len + 1);
-	check_failure(0, dup, 2, all);
-	ft_memcpy(dup, start, len);
-	dup[len] = '\0';
-	update_map(all, found);
-	return (dup);    
+	return (temp);    
 }
 
 char	*finder(t_all *all, char *str)
@@ -124,7 +180,7 @@ void only_digit(t_all *all, char **split, char type)
 		j = 0;
 		while(split[i][j])
 		{
-			if (!ft_isdigit(split[i][j]))
+			if (!ft_isdigit(split[i][j]) && split[i][j] != ' ')
 			{
 				clean_2d_char_array(split);
 				ft_putchar_fd(type, 2);
