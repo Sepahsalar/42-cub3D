c7ecf13be89333f4b4b89ae570ffbcf39b198991
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-08-15 14:20:30 +0300

    fill strips utils3 cleaned up

--- src/fill_strips_utils3.c
+++ src/fill_strips_utils3.c
@@ -3,52 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   fill_strips_utils3.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asohrabi <asohrabi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 19:22:17 by asohrabi          #+#    #+#             */
-/*   Updated: 2024/08/14 19:33:32 by asohrabi         ###   ########.fr       */
+/*   Updated: 2024/08/15 14:18:59 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
 
-void	fill_index_strips(t_all *all)
+typedef struct s_current_wall
 {
-	t_strip	*node;
-	int		index;
 	char	wall;
 	int		x_winner;
 	int		y_winner;
+	int		index;
+}			t_current_wall;
 
-	index = 0;
-	node = all->strip;
-	wall = node->wall;
-	x_winner = node->x_winner;
-	y_winner = node->y_winner;
-	while (node)
-	{
-		if (node->wall == wall && (((node->wall == 'E' || node->wall == 'W')
-					&& same(node->x_winner, x_winner)) || ((node->wall == 'N'
-						|| node->wall == 'S') && same(node->y_winner,
-						y_winner))))
-			node->index = index;
-		else
-		{
-			wall = node->wall;
-			x_winner = node->x_winner;
-			y_winner = node->y_winner;
-			index = 0;
-			node->index = index;
-		}
-		index++;
-		node = node->next;
-	}
-	node = all->strip;
-	while (node)
-		node = node->next;
+int this_node_does_not_belong_to_current_wall(t_strip *node, t_current_wall current_wall)
+{
+	if ((node->wall == current_wall.wall)
+			&& (((node->wall == 'E' || node->wall == 'W') && same(node->x_winner, current_wall.x_winner))
+			|| ((node->wall == 'N'	|| node->wall == 'S') && same(node->y_winner, current_wall.y_winner))))
+		return (0);
+	else
+		return (1);
 }
 
-void	update_strips(t_all *all)
+t_current_wall	make_this_node_start_of_wall(t_strip *node)
+{
+	t_current_wall	current_wall;
+
+	current_wall.wall = node->wall;
+	current_wall.x_winner = node->x_winner;
+	current_wall.y_winner = node->y_winner;
+	current_wall.index = 0;
+	return (current_wall);
+}
+
+
+void	fill_index_strips(t_all *all)
+{
+	t_strip			*node;
+	t_current_wall	current_wall;
+
+	node = all->strip;
+	current_wall = make_this_node_start_of_wall(node);
+	while (node)
+	{
+		if (this_node_does_not_belong_to_current_wall(node, current_wall))
+			current_wall = make_this_node_start_of_wall(node);
+		node->index = current_wall.index;
+		current_wall.index++;
+		node = node->next;
+	}
+}
+
+void make_floor_linear(t_all *all)
 {
 	t_strip	*node;
 	t_strip	*last;
@@ -68,6 +79,15 @@ void	update_strips(t_all *all)
 		}
 		node = node->next;
 	}
+}
+
+void make_wall_and_ceil_linear(t_all *all)
+{
+	t_strip	*node;
+	t_strip	*last;
+	t_strip	*first;
+	int		index;
+
 	node = all->strip;
 	while (node)
 	{
@@ -83,4 +103,10 @@ void	update_strips(t_all *all)
 		}
 		node = node->next;
 	}
+}
+
+void	update_strips(t_all *all)
+{
+	make_floor_linear(all);
+	make_wall_and_ceil_linear(all);
 }
