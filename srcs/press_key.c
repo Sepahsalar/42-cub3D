70d71ff535eb6b86a3bb8308bd6d960acd702795
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-08-22 11:44:00 +0300

    Added the ability of repeating moves

--- srcs/press_key.c
+++ srcs/press_key.c
@@ -6,7 +6,7 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:23:58 by asohrabi          #+#    #+#             */
-/*   Updated: 2024/08/22 11:12:25 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/08/22 11:38:09 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,46 +53,25 @@ static void	turn_press(void *param, char c)
 static void	turn_repeat(void *param, char c)
 {
 	t_all	*all;
-	// int		target;
-	// int time;
 
 	all = (t_all *)param;
 	if (all->started_button == 0)
 	{
-		// all->start_time = ft_timestamp_ms(all);
-		// all->started_button = 1;
-		// clean_strips(all);
-		// mlx_delete_image(all->window, all->image);
-		// if (c == 'R')
-		// 	all->angle += TURN_INTERVAL;
-		// else
-		// 	all->angle -= TURN_INTERVAL;
-		// all->angle = under_full_circle(all->angle);
-		// fill_strips(all);
 		turn_press(param, c);
 		all->start_time = ft_timestamp_ms(all);
 		all->current_time  = all->start_time;
 		all->started_button = 1;
 	}
-	// target = 2000;
 	all->current_time =ft_timestamp_ms(all);
 	if (all->current_time - all->start_time >= RENDER_INTERVAL)
 	{
 		turn_press(param, c);
-		// clean_strips(all);
-		// mlx_delete_image(all->window, all->image);
-		// if (c == 'R')
-		// 	all->angle += TURN_INTERVAL;
-		// else
-		// 	all->angle -= TURN_INTERVAL;
-		// all->angle = under_full_circle(all->angle);
-		// fill_strips(all);
 		all->start_time = ft_timestamp_ms(all);
 		all->current_time  = all->start_time;
 	}
 }
 
-static void	move(void *param, char c)
+static void	move_press(void *param, char c)
 {
 	t_all	*all;
 	double	new_x;
@@ -112,22 +91,59 @@ static void	move(void *param, char c)
 	conditional_move(all, new_x, new_y);
 }
 
+static void	move_repeat(void *param, char c)
+{
+	t_all	*all;
+
+	all = (t_all *)param;
+	if (all->started_button == 0)
+	{
+		move_press(param, c);
+		all->start_time = ft_timestamp_ms(all);
+		all->current_time  = all->start_time;
+		all->started_button = 1;
+	}
+	all->current_time =ft_timestamp_ms(all);
+	if (all->current_time - all->start_time >= RENDER_INTERVAL)
+	{
+		move_press(param, c);
+		all->start_time = ft_timestamp_ms(all);
+		all->current_time  = all->start_time;
+	}
+}
+
 void	press_key(mlx_key_data_t keydata, void *param)
 {
 	t_all	*all;
 
 	all = (t_all *)param;
-	if ((keydata.key == MLX_KEY_W || keydata.key == MLX_KEY_UP)
-		&& keydata.action == MLX_PRESS)
-		move(param, 'W');
-	else if ((keydata.key == MLX_KEY_S || keydata.key == MLX_KEY_DOWN)
-			&& keydata.action == MLX_PRESS)
-		move(param, 'S');
+	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS)
+		move_press(param, 'W');
+	else if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS)
+		move_press(param, 'S');
 	else if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS)
-		move(param, 'A');
+		move_press(param, 'A');
 	else if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS)
-		move(param, 'D');
-		
+		move_press(param, 'D');
+	
+	else if (keydata.key == MLX_KEY_W && keydata.action == MLX_REPEAT)
+		move_repeat(param, 'W');
+	else if (keydata.key == MLX_KEY_S && keydata.action == MLX_REPEAT)
+		move_repeat(param, 'S');
+	else if (keydata.key == MLX_KEY_A && keydata.action == MLX_REPEAT)
+		move_repeat(param, 'A');
+	else if (keydata.key == MLX_KEY_D && keydata.action == MLX_REPEAT)
+		move_repeat(param, 'D');
+	
+	else if (keydata.key == MLX_KEY_W && keydata.action == MLX_RELEASE)
+		all->started_button = 0;
+	else if (keydata.key == MLX_KEY_S && keydata.action == MLX_RELEASE)
+		all->started_button = 0;
+	else if (keydata.key == MLX_KEY_A && keydata.action == MLX_RELEASE)
+		all->started_button = 0;
+	else if (keydata.key == MLX_KEY_D && keydata.action == MLX_RELEASE)
+		all->started_button = 0;
+	
 	else if (keydata.key == MLX_KEY_LEFT && keydata.action == MLX_PRESS)
 		turn_press(param, 'L');
 	else if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_PRESS)
